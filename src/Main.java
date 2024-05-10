31e7318c0776f1b1ed84281449f4fb4a4edd9e63
Kirles <k.bushtets@gmail.com>
2024-05-10 18:21:01 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import frames.AuthFrame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static Connection conn;
+
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:sqlite:warehouse.db";
+        conn = DriverManager.getConnection(url);
+        AuthFrame.frame();
+
+    }
+}
