9e2cd6b7ad95ef58ef3b179e2e673034238263e6
Kirles <k.bushtets@gmail.com>
2024-05-10 18:34:09 +0300

    add login frame

---
+++ src/frames/LoginFrame.java
@@ -0,0 +1,46 @@
+package frames;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame {
+
+    private static JButton loginButton;
+    private static JTextField emailField;
+
+    public static void main(String[] args) {
+        frame();
+    }
+
+    public static void frame() {
+        LoginFrame rf = new LoginFrame();
+        rf.setTitle("Warehouse");
+        rf.setResizable(false);
+        rf.setSize(500, 250);
+
+        rf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        ImagePanel panel = new ImagePanel("source/nebo.jpg");
+
+        emailField = new JTextField();
+        emailField.setBounds(125, 75, 250, 25);
+        emailField.setText("Enter the email");
+
+        loginButton = new JButton("Увійти");
+        loginButton.setBounds(175, 150, 150, 25);
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        rf.add(emailField);
+        rf.add(loginButton);
+        rf.add(panel);
+
+        rf.setVisible(true);
+    }
+}
+
